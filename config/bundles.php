0033ef9e0113491f13878c4e855c910ea51449aa
Mickey3d <mickey.welensky@gmail.com>
2018-04-20 01:15:29 +0200

    [SWIFTMAILLER BUNDLE] Configuration Updated with symfony official recipes

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Hillrange\CKEditor\HillrangeCKEditorBundle::class => ['all' => true],
     App\Surikat\CommunityBundle\SurikatCommunityBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
